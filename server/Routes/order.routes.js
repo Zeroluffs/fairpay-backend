4bea43eef8665ebd407b98d542fef6c756a86dd4
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-07-16 15:41:28 -0500

    billing route

--- server/Routes/order.routes.js
+++ server/Routes/order.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const order = require("../controllers/order.controller");
 
 router.post("/:id", order.addOrder);
+router.get("/bill/:id", order.getBilling);
 
 module.exports = router;
--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -3,20 +3,31 @@ const TableGroup = require("../models/TableGroup");
 
 const orderCtrl = {};
 
-orderCtrl.addOrder = async (req,res) =>{
-    const newOrder = new Order({
-        name: req.body.name,
-        product: req.body.product,
-        price: req.body.price,
-        groupID: req.params.id
-    })
+orderCtrl.addOrder = async (req, res) => {
+  const newOrder = new Order({
+    name: req.body.name,
+    product: req.body.product,
+    price: req.body.price,
+    groupID: req.params.id,
+  });
 
-    await newOrder.save();
+  await newOrder.save();
 
-    await TableGroup.findByIdAndUpdate(req.params.id,   { $push: { orders: newOrder } },
-        { new: true, useFindAndModify: false })
+  await TableGroup.findByIdAndUpdate(
+    req.params.id,
+    { $push: { orders: newOrder } },
+    { new: true, useFindAndModify: false }
+  );
 
-        res.send(newOrder)
-}
+  res.send(newOrder);
+};
+
+orderCtrl.getBilling = async (req, res) => {
+  const ordersInGroup = await TableGroup
+    .findById(req.params.id)
+    .populate("orders");
+  let orders = await ordersInGroup.orders;
+  res.json(orders);
+};
 
 module.exports = orderCtrl;
